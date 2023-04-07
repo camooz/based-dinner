79c9851441c9042dd5597e201eb9c7fa13d3fedd
Michel de Montaigne <bertrandrussell78@protonmail.com>
2023-04-07 20:05:08 -0300

    Organize code and change terms to English

--- dinner.py
+++ dinner.py
@@ -9,6 +9,12 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+allRecipes = {
+    'Title': [],
+    'Tags': [],
+    'URL': []
+}
+
 # function for request
 def get():
     r = requests.get("https://based.cooking", timeout=5)
@@ -28,27 +34,21 @@ def output():
     You can check out the recipe here: {fortune[0,2]}
     ''')
 
-allReceitas = {
-    'Title': [],
-    'Tags': [],
-    'URL': []
-}
-
 soup = BeautifulSoup(get(), "html.parser")
 
 # iterate over data and append to dict
-receitas = soup.find(id="artlist")
-for receita in receitas.find_all('li'): 
-    titulo = receita.get_text() # Pega o título da receita
-    tags = receita.get('data-tags').strip('[]').split() # Pega as tags da receita
-    for link in receita.find_all('a'):
-        url = link.get('href') # Pega o link da receita
-        allReceitas['Title'].append(titulo)
-        allReceitas['Tags'].append(tags)
-        allReceitas['URL'].append(url)
+recipes = soup.find(id="artlist")
+for recipe in recipes.find_all('li'): 
+    title = recipe.get_text() # gets recipe's title
+    tags = recipe.get('data-tags').strip('[]').split() # gets recipe's tags
+    for link in recipe.find_all('a'):
+        url = link.get('href') # gets recipe's url
+        allRecipes['Title'].append(title)
+        allRecipes['Tags'].append(tags)
+        allRecipes['URL'].append(url)
 
 # structure data using pandas
-df = pd.DataFrame(allReceitas)
+df = pd.DataFrame(allRecipes)
 
 fortune = df.sample().to_numpy()
 
